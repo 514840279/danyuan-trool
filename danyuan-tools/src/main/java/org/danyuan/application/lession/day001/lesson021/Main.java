5d82ddbc576566e753f82ebcfe2dde58c7a506de
王滕辉 <514840279@qq.com>
2021-01-09 12:15:46 +0800

    1

--- danyuan-tools/src/main/java/org/danyuan/application/lession/day001/lesson021/Main.java
+++ danyuan-tools/src/main/java/org/danyuan/application/lession/day001/lesson021/Main.java
@@ -4,9 +4,10 @@
 package org.danyuan.application.lession.day001.lesson021;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TitledPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 /**
@@ -35,12 +36,20 @@ public class Main extends Application {
 		Button btn7 = new Button("button7");
 		Button btn8 = new Button("button8");
 
-		TitledPane titledPane = new TitledPane();
+		TilePane tilePane = new TilePane();
 
-		titledPane.getChildrenUnmodifiable().addAll(btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8);
-		Scene scene = new Scene(titledPane);
+		tilePane.getChildren().addAll(btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8);
+
+		tilePane.setVgap(10); // 间距
+		tilePane.setHgap(10); //
+
+		tilePane.setPadding(new Insets(10));
+		TilePane.setMargin(btn1,new Insets(20)); // 导致其他空间都有间距
+
+
+		Scene scene = new Scene(tilePane);
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("TextFlow布局类");
+		primaryStage.setTitle(" TilePane布局类");
 		primaryStage.setWidth(400);
 		primaryStage.setHeight(400);
 		primaryStage.show();
